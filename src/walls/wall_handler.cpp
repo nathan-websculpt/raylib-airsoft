88cf8b137ecb40eb99bf34c3ef1101a32fde8d26
nathan-websculpt <websculpt.nathan@gmail.com>
2025-09-12 09:38:53 -0500

    brought in the polymorphic walls

---
+++ src/walls/wall_handler.cpp
@@ -0,0 +1,12 @@
+#include "../../include/walls/wall_handler.h"
+
+void WallHandler::AddWall(std::unique_ptr<Wall> wall) {
+    m_walls.emplace_back(std::move(wall)); // emplace_back constructs the object in-place, directly in the vector's memory
+}
+
+void WallHandler::DrawWalls(bool debug) const {
+    for (const std::unique_ptr<Wall>& wall : m_walls) {
+        wall->Draw();
+        if (debug) wall->DrawDebug();
+    }
+}
